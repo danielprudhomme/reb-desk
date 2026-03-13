0a2d854d04bc8abe83774ab944efe193cbaec898
Daniel PRUD'HOMME <daniel.prudhomme.pro@gmail.com>
2026-03-13 15:54:24 +0100

    update eslint

---
+++ front/eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['**/node_modules/**', '**/.angular/**', '**/dist/**', '**/build/**', '**/.vite/**'],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    files: ['**/*.{ts,js}'],
+  },
+
+  prettier,
+];
--- front/src/app/core/graphql/graphql.service.ts
+++ front/src/app/core/graphql/graphql.service.ts
@@ -6,6 +6,7 @@ import { RequestState } from '../models/request-state.model';
 export abstract class GraphQlService {
   private apollo = inject(Apollo);
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   query$<T>(query: TypedDocumentNode<any, any>, key: string): Observable<RequestState<T>> {
     return this.apollo.watchQuery<{ [key]: T }>({ query }).valueChanges.pipe(
       map((result) => ({
