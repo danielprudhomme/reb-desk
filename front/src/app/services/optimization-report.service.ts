93ff88ef5a8eede4f273e52dc9ccc50e55498d2c
Daniel PRUD'HOMME <daniel.prudhomme.pro@gmail.com>
2026-03-13 15:45:56 +0100

    clean

--- front/src/app/services/optimization-report.service.ts
+++ front/src/app/services/optimization-report.service.ts
@@ -1,7 +1,6 @@
 import { inject, Injectable } from '@angular/core';
-import { Apollo } from 'apollo-angular';
 import { OptimizationReport } from '@shared/models/optimization-report';
-import { map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { GET_OPTIMIZATION_REPORTS } from './optimization-report.graphql';
 import { HttpClient } from '@angular/common/http';
 import { GraphQlService } from '../core/graphql/graphql.service';
