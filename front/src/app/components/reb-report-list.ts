1b3681ae91f19c840e5fcf56b9943e3aabe5434e
Daniel PRUD'HOMME <daniel.prudhomme.pro@gmail.com>
2026-03-21 22:22:03 +0100

    add sort etc to report list

--- front/src/app/components/reb-report-list.ts
+++ front/src/app/components/reb-report-list.ts
@@ -1,65 +1,74 @@
-import { Component, inject } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, computed, inject, viewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { RebReportService } from '../services/reb-report.service';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { RebReport } from '../core/models/reb-report';
 
 @Component({
   selector: 'app-reb-report-list',
-  imports: [RouterLink, MatButtonModule, MatTableModule],
+  imports: [RouterLink, MatButtonModule, MatTableModule, MatSortModule],
   template: `
-    @if (reports(); as reports) {
-      <table mat-table [dataSource]="reports" class="mat-elevation-z8">
+    <div class="h-full overflow-auto">
+      <table mat-table [dataSource]="dataSource()" matSort>
         <ng-container matColumnDef="expert">
-          <th mat-header-cell *matHeaderCellDef>Expert</th>
+          <th mat-header-cell *matHeaderCellDef mat-sort-header>Expert</th>
           <td mat-cell *matCellDef="let report">{{ report.expert }}</td>
         </ng-container>
 
         <ng-container matColumnDef="symbol">
-          <th mat-header-cell *matHeaderCellDef>Symbol</th>
+          <th mat-header-cell *matHeaderCellDef mat-sort-header>Symbol</th>
           <td mat-cell *matCellDef="let report">{{ report.symbol }}</td>
         </ng-container>
 
         <ng-container matColumnDef="timeframe">
-          <th mat-header-cell *matHeaderCellDef>Timeframe</th>
+          <th mat-header-cell *matHeaderCellDef mat-sort-header>Timeframe</th>
           <td mat-cell *matCellDef="let report">{{ report.timeframe }}</td>
         </ng-container>
 
         <ng-container matColumnDef="startDate">
-          <th mat-header-cell *matHeaderCellDef>Start Date</th>
+          <th mat-header-cell *matHeaderCellDef mat-sort-header>Start Date</th>
           <td mat-cell *matCellDef="let report">{{ report.startDate }}</td>
         </ng-container>
 
         <ng-container matColumnDef="shortTerm">
-          <th mat-header-cell *matHeaderCellDef>Short Term</th>
+          <th mat-header-cell *matHeaderCellDef mat-sort-header>Short Term</th>
           <td mat-cell *matCellDef="let report">
             {{ report.shortTermCount }} x {{ report.shortTermDuration }} {{ report.shortTermUnit }}
           </td>
         </ng-container>
 
         <ng-container matColumnDef="longTerm">
-          <th mat-header-cell *matHeaderCellDef>Long Term</th>
+          <th mat-header-cell *matHeaderCellDef mat-sort-header>Long Term</th>
           <td mat-cell *matCellDef="let report">
             {{ report.longTermDuration }} {{ report.longTermUnit }}
           </td>
         </ng-container>
 
         <ng-container matColumnDef="actions">
-          <th mat-header-cell *matHeaderCellDef></th>
+          <th mat-header-cell *matHeaderCellDef mat-sort-header></th>
           <td mat-cell *matCellDef="let report">
             <button matButton="filled" [routerLink]="[report.id, 'analyze']">Analyze</button>
           </td>
         </ng-container>
 
-        <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+        <tr mat-header-row *matHeaderRowDef="displayedColumns; sticky: true"></tr>
         <tr mat-row *matRowDef="let row; columns: displayedColumns"></tr>
       </table>
-    }
+    </div>
   `,
 })
 export class RebReportList {
+  private sort = viewChild.required(MatSort);
   private rebReportService = inject(RebReportService);
-  reports = this.rebReportService.reports;
+
+  dataSource = computed(() => {
+    const dataSource = new MatTableDataSource<RebReport>(this.rebReportService.reports());
+    dataSource.sort = this.sort();
+    return dataSource;
+  });
+
   displayedColumns: string[] = [
     'expert',
     'symbol',
