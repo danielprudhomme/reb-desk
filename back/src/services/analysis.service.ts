96b7beff2f8c6216d79983204a046a9dcdab84cc
Daniel PRUD'HOMME <dprudhomme741@gmail.com>
2026-04-12 11:39:05 +0200

    refactor and clean functions

--- back/src/services/analysis.service.ts
+++ back/src/services/analysis.service.ts
@@ -51,19 +51,26 @@ export function analyzePasses(
   passes: BacktestPass[],
   thresholds: BacktestThreshold[],
 ): BacktestPassAnalysis[] {
+  const { analyzedPasses, valuesByType } = runChecks(report, passes, thresholds);
+  computeScore(analyzedPasses, thresholds, valuesByType);
+  return analyzedPasses;
+}
+
+export function runChecks(
+  report: RebReport,
+  passes: BacktestPass[],
+  thresholds: BacktestThreshold[],
+): {
+  analyzedPasses: BacktestPassAnalysis[];
+  valuesByType: Record<BacktestThresholdType, { worstValues: number[]; min: number; max: number }>;
+} {
   const valuesByType: Record<
     BacktestThresholdType,
     { worstValues: number[]; min: number; max: number }
   > = {} as Record<BacktestThresholdType, { worstValues: number[]; min: number; max: number }>;
 
-  const checkPasses: Record<string, BacktestThresholdCheck[]> = {} as Record<
-    string,
-    BacktestThresholdCheck[]
-  >;
-
-  // premier passage, pour récupérer toutes les valeurs (ensuite on compare les valeurs entre elles)
-  passes.forEach((pass) => {
-    thresholds.forEach((threshold) => {
+  const analyzedPasses: BacktestPassAnalysis[] = passes.map((pass) => {
+    const checks: BacktestThresholdCheck[] = thresholds.map((threshold) => {
       const compute = BACKTEST_THRESHOLD_COMPUTE[threshold.type];
       const passValues = compute(pass, report.capital);
 
@@ -91,7 +98,7 @@ export function analyzePasses(
         }
       }
 
-      const check: BacktestThresholdCheck = {
+      return {
         type: threshold.type,
         ok,
         worstValue,
@@ -99,107 +106,12 @@ export function analyzePasses(
         requiredRate: threshold.passRate,
         score: 0,
       };
-
-      (checkPasses[pass.id] ??= []).push(check);
     });
-  });
-
-  return passes.map((pass) => {
-    const checkByType = Object.fromEntries(checkPasses[pass.id].map((c) => [c.type, c]));
-
-    const checks = thresholds.map((threshold) => {
-      const check = checkByType[threshold.type];
-
-      const { min, max } = valuesByType[threshold.type];
-
-      if (!min || min === max) {
-        check.score = 0;
-        return check;
-      }
-
-      // ===== CAS 1 : OK =====
-      if (check.ok) {
-        let normalized =
-          threshold.operator === '>'
-            ? (check.worstValue - min) / (max - min)
-            : (max - check.worstValue) / (max - min);
-
-        normalized = Math.max(0, Math.min(1, normalized));
-
-        check.score = 0.5 + 0.5 * Math.pow(normalized, 2);
-        return check;
-      }
-
-      // ===== CAS 2 : MARGE (uniquement pour rate) =====
-      const usedValue = valueTypeConst.BACKTEST_THRESHOLD_VALUE_TYPE[threshold.type];
-      if (usedValue === 'rate') {
-        const margin = threshold.passRate * 0.1;
-        const minAcceptableRate = threshold.passRate - margin;
-
-        if (check.rate >= minAcceptableRate) {
-          // normalisation dans la zone [minAcceptableRate ; passRate]
-          const normalized =
-            (check.rate - minAcceptableRate) / (threshold.passRate - minAcceptableRate);
-
-          // score entre 0 → 0.5
-          check.score = 0.5 * Math.pow(normalized, 2);
-
-          return check;
-        }
-      }
-
-      // ===== CAS 2 BIS : MARGE pour VALUE =====
-      if (usedValue === 'value' && check.rate > threshold.passRate * 0.5) {
-        const marginPercent = 0.1; // 10%
-        const margin = threshold.value * marginPercent;
-
-        if (threshold.operator === '<') {
-          const maxAcceptable = threshold.value + margin;
-
-          if (check.worstValue <= maxAcceptable) {
-            const normalized =
-              (maxAcceptable - check.worstValue) / (maxAcceptable - threshold.value);
-
-            check.score = 0.5 * Math.pow(Math.max(0, Math.min(1, normalized)), 2);
-
-            return check;
-          }
-        }
-
-        if (threshold.operator === '>') {
-          const minAcceptable = threshold.value - margin;
-
-          if (check.worstValue >= minAcceptable) {
-            const normalized =
-              (check.worstValue - minAcceptable) / (threshold.value - minAcceptable);
-
-            check.score = 0.5 * Math.pow(Math.max(0, Math.min(1, normalized)), 2);
-
-            return check;
-          }
-        }
-      }
-
-      // ===== CAS 3 : FAIL =====
-      check.score = 0;
-      return check;
-    });
-
-    const ok = checks.every((c) => c.score > 0);
-
-    const weightMap = Object.fromEntries(thresholds.map((t) => [t.type, t.weight ?? 1]));
-    const totalWeight = checks.reduce((acc, c) => acc + weightMap[c.type], 0);
-    const score = checks.reduce((acc, c) => acc + c.score * weightMap[c.type], 0) / totalWeight;
-
-    const hasCriticalFail = checks.some(
-      (c) => !c.ok && (thresholds.find((t) => t.type === c.type)?.weight ?? 1) >= 3,
-    );
-    const finalScore = !ok ? (hasCriticalFail ? score * 0.5 : score * 0.75) : score;
 
     return {
-      ok,
+      ok: false,
       checks,
-      score: finalScore,
+      score: 0,
       reportId: report.id,
       passId: pass.id,
       parameters: pass.parameters,
@@ -215,6 +127,116 @@ export function analyzePasses(
       longTermUnit: report.longTermUnit,
     };
   });
+
+  return { analyzedPasses, valuesByType };
+}
+
+export function computeScore(
+  analyzedPasses: BacktestPassAnalysis[],
+  thresholds: BacktestThreshold[],
+  valuesByType: Record<BacktestThresholdType, { worstValues: number[]; min: number; max: number }>,
+) {
+  const thresholdsMap: Record<BacktestThresholdType, BacktestThreshold> = Object.fromEntries(
+    thresholds.map((t) => [t.type, t]),
+  ) as Record<BacktestThresholdType, BacktestThreshold>;
+
+  analyzedPasses.forEach((pass) => {
+    pass.checks.forEach((check) => {
+      const { min, max } = valuesByType[check.type];
+
+      if (!min || min === max) {
+        return;
+      }
+
+      const threshold = thresholdsMap[check.type];
+
+      // ===== CAS 1 : OK =====
+      if (check.ok) {
+        computeScoreWhenCheckOk(check, threshold, min, max);
+      }
+      // ===== CAS 2 : MARGE (uniquement pour rate) =====
+      else if (valueTypeConst.BACKTEST_THRESHOLD_VALUE_TYPE[threshold.type] === 'rate') {
+        computeScoreNotOkForRate(check, threshold);
+      }
+      // ===== CAS 2 BIS : MARGE pour VALUE =====
+      else {
+        computeScoreNotOkForValue(check, threshold);
+      }
+    });
+
+    const ok = pass.checks.every((c) => c.score > 0);
+
+    const weightMap = Object.fromEntries(thresholds.map((t) => [t.type, t.weight ?? 1]));
+    const totalWeight = pass.checks.reduce((acc, c) => acc + weightMap[c.type], 0);
+    const score =
+      pass.checks.reduce((acc, c) => acc + c.score * weightMap[c.type], 0) / totalWeight;
+
+    const hasCriticalFail = pass.checks.some(
+      (c) => !c.ok && (thresholds.find((t) => t.type === c.type)?.weight ?? 1) >= 3,
+    );
+    const finalScore = !ok ? (hasCriticalFail ? score * 0.5 : score * 0.75) : score;
+
+    pass.ok = ok;
+    pass.score = finalScore;
+  });
+}
+
+export function computeScoreWhenCheckOk(
+  check: BacktestThresholdCheck,
+  threshold: BacktestThreshold,
+  min: number,
+  max: number,
+) {
+  let normalized =
+    threshold.operator === '>'
+      ? (check.worstValue - min) / (max - min)
+      : (max - check.worstValue) / (max - min);
+
+  normalized = Math.max(0, Math.min(1, normalized));
+
+  check.score = 0.5 + 0.5 * Math.pow(normalized, 2);
+}
+
+export function computeScoreNotOkForRate(
+  check: BacktestThresholdCheck,
+  threshold: BacktestThreshold,
+) {
+  const margin = threshold.passRate * 0.1;
+  const minAcceptableRate = threshold.passRate - margin;
+
+  if (check.rate >= minAcceptableRate) {
+    const normalized = (check.rate - minAcceptableRate) / (threshold.passRate - minAcceptableRate);
+
+    // score entre 0 → 0.5
+    check.score = 0.5 * Math.pow(normalized, 2);
+  }
+}
+
+export function computeScoreNotOkForValue(
+  check: BacktestThresholdCheck,
+  threshold: BacktestThreshold,
+) {
+  if (check.rate < threshold.passRate * 0.5) return;
+
+  const margin = threshold.value * 0.1; // 10%
+
+  if (threshold.operator === '<') {
+    const maxAcceptable = threshold.value + margin;
+
+    if (check.worstValue <= maxAcceptable) {
+      const normalized = (maxAcceptable - check.worstValue) / (maxAcceptable - threshold.value);
+
+      check.score = 0.5 * Math.pow(Math.max(0, Math.min(1, normalized)), 2);
+    }
+  } else {
+    const minAcceptable = threshold.value - margin;
+
+    if (check.worstValue >= minAcceptable) {
+      const normalized = (check.worstValue - minAcceptable) / (threshold.value - minAcceptable);
+
+      check.score = 0.5 * Math.pow(Math.max(0, Math.min(1, normalized)), 2);
+    }
+  }
 }
 
 // A envoyer dans la requête - pas hardcodé
